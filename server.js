70dd86ab592c6422b20935c87e54db84f78db71a
rockysingh3 <rockygsingh@icloud.com>
2018-02-28 01:24:14 -0500

    Added the server and the Get route

--- server.js
+++ server.js
@@ -1,2 +1,43 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+
+app.set("view engine", "ejs");
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+app.get('/detials', (req, res) => {
+  res.render("detials");
+});
+
+
+
+app.get('/home', (req, res) => {
+    let query = req.query.search;
+    let urlWithoutId = "http://www.omdbapi.com/?s=" + query + "&plot=true&&apikey=c3090b9a";
+    request(urlWithoutId, (error, response, body) => {
+      if(!error && response.statusCode == 200){
+        let apiResults = JSON.parse(body);
+        //console.log(apiResults);
+          res.render("home", {data: apiResults});
+        }
+      });
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("The app is running on port 3000");
+});
