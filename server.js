4e423070b5565671e8d1dd710808997f2a177c5d
rockysingh3 <rockygsingh@icloud.com>
2018-02-28 05:18:26 -0500

    Changed the main route to home

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home");
 });
 
 
 
 // makes the request to the api
-app.get('/home', (req, res) => {
+app.get('/showMovies', (req, res) => {
     let query = req.query.search;
     let urlWithoutId = "http://www.omdbapi.com/?s=" + query + "&plot=true&&apikey=c3090b9a";
     request(urlWithoutId, (error, response, body) => {
       if(!error && response.statusCode == 200){
         let apiResults = JSON.parse(body);
         //console.log(apiResults);
-          res.render("home", {data: apiResults});
+          res.render("showMovies", {data: apiResults});
         }
       });
 });
